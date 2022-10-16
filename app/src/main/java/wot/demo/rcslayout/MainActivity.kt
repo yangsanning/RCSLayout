be079c4ec40d3abdc6ccde5b9e1c22efe23c1c65
杨三宁 <ysn_2020@163.com>
2022-10-16 19:16:26 +0800

    [ +] init: 初始化仓库

---
+++ app/src/main/java/wot/demo/rcslayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package wot.demo.rcslayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wot.view.rcslayout.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
